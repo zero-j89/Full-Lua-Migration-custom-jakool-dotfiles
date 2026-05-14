be885896b9ab549f519ce362697608368c6d7098
GroundZeR0ML <scrspk8@gmail.com>
2026-05-14 14:52:50 -0400

    Update Startup_Apps.lua

--- UserConfigs/Startup_Apps.lua
+++ UserConfigs/Startup_Apps.lua
@@ -1,7 +1,7 @@
 hl.on("hyprland.start", function()
-  hl.exec_cmd("sh -c 'sleep 3 && lact gui --startminimized &'")
-  hl.exec_cmd('sh -c \'sleep 5 && openrgb --profile "helldivers" &\'')
+  hl.exec_cmd("sh -c 'program-name &'")
+ 
 
   -- Disabled:
-  -- hl.exec_cmd('sh -c \'sleep 8 && trcc gui theme-load "Custom_zer0" &\'')
+  -- hl.exec_cmd('sh -c \'redacted "Custom_zer0" &\'')
 end)
