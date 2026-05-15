77023c143ec200d6f1136ee8dfdc60f57b972583
GroundZeR0ML <scrspk8@gmail.com>
2026-05-15 16:01:51 -0400

    added new term for drop temrinal

--- UserConfigs/01-UserDefaults.lua
+++ UserConfigs/01-UserDefaults.lua
@@ -5,6 +5,7 @@ _G.edit = os.getenv("EDITOR") or "nano"
 -- Default applications
 _G.term = "kitty"
 _G.files = "thunar"
+_G.term = "kitty --class dropdown-terminal"
 
 -- Search engine
 _G.Search_Engine = "https://www.google.com/search?q={}"
