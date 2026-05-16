13f14d0db2c25034a644d4a60d5432d280b40855
GroundZeR0ML <scrspk8@gmail.com>
2026-05-16 08:56:33 -0400

    fixed User startups

---
+++ UserConfigs/UserStartup_Apps.lua
@@ -0,0 +1,12 @@
+hl.on("hyprland.start", function()
+   hl.exec_cmd('openrgb --startminimized --profile "helldivers"')
+end)
+
+-- end)
+
+ --  hl.exec_cmd("sh -c 'sleep 3 && lact gui --startminimized &'")
+ --  hl.exec_cmd('sh -c \'sleep 5 && openrgb --profile "helldivers" &\'')
+
+  -- Disabled:
+  --  hl.exec_cmd('sh -c \'sleep 8 && trcc gui theme-load "Custom_zer0" &\'')
+
