7b41b19a09b7f2e637d1d04175fd090927bbac91
GroundZeR0ML <scrspk8@gmail.com>
2026-05-16 01:19:46 -0400

    fixed window rules
    also added custom steam and game launcher rules

--- configs/WindowRules.lua
+++ configs/WindowRules.lua
@@ -208,3 +208,57 @@ lr({ match = { namespace = "noctalia-background-.*$" }, ignore_alpha = 0.5 })
 --   float = true,
 --   center = true,
 -- })
+
+wr({
+  name = "dropdown-terminal",
+  match = {
+    class = "^(dropdown-terminal)$",
+  },
+  float = true,
+  size = "1200 700",
+  move = "680 100",
+})
+
+-- Steam main client/library on workspace 5
+wr({
+  name = "steam-main-workspace-5",
+  match = {
+    class = "^([Ss]team)$",
+    title = "^([Ss]team|Steam)$",
+  },
+  workspace = "5 silent",
+  float = true,
+  size = "1280 800",
+  move = "1264 625",
+})
+-- Steam child dialogs/popups stay floating, centered
+wr({
+  name = "steam-popups-float",
+  match = {
+    class = "^([Ss]team)$",
+    title = "negative:^([Ss]team|Steam)$",
+  },
+  float = true,
+  center = true,
+})
+
+-- Steam games on workspace 4
+wr({
+  name = "steam-games-workspace-4",
+  match = {
+    class = "^(steam_app_[0-9]+)$",
+  },
+  workspace = "4 silent",
+  no_blur = true,
+})
+
+-- Gamescope on workspace 4
+wr({
+  name = "gamescope-workspace-4",
+  match = {
+    class = "^(gamescope)$",
+  },
+  workspace = "4 silent",
+  no_blur = true,
+})
+
