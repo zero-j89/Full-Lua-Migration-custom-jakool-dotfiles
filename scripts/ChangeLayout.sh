32a944104cc95074fe1708009232e4e5bf129898
GroundZeR0ML <264713863+zero-j89@users.noreply.github.com>
2026-05-25 09:22:38 -0400

    fixed layouts

--- scripts/ChangeLayout.sh
+++ scripts/ChangeLayout.sh
@@ -1,36 +1,21 @@
 #!/usr/bin/env bash
+# Toggle only master/dwindle. Do not touch keybinds.
+# GZML toggle
 
 notif="$HOME/.config/swaync/images/ja.png"
-
-LAYOUT=$(hyprctl -j getoption general:layout | jq -r '.str')
-
-# Reverse layout value to reuse toggle logic. So layouts don't get swapped initially.
-if [ "$1" = "init" ]; then
-  if [ "$LAYOUT" = "master" ]; then
-    LAYOUT="dwindle"
-  else
-    LAYOUT="master"
-  fi
-fi
+LAYOUT="$(hyprctl -j getoption general:layout | jq -r '.str')"
 
 case "$LAYOUT" in
-"master")
-  hyprctl eval "general:layout = dwindle"
-  hyprctl eval "unbind = SUPER,J"
-  hyprctl eval "unbind = SUPER,K"
-  hyprctl eval "bind = SUPER,J,cyclenext"
-  hyprctl eval "bind = SUPER,K,cyclenext,prev"
-  hyprctl eval "bind = SUPER,O,togglesplit"
-  notify-send -e -u low -i "$notif" " Dwindle Layout"
-  ;;
-"dwindle")
-  hyprctl eval "general:layout = master"
-  hyprctl eval "unbind = SUPER,J"
-  hyprctl eval "unbind = SUPER,K"
-  hyprctl eval "unbind = SUPER,O"
-  hyprctl eval "bind = SUPER,J,layoutmsg,cyclenext"
-  hyprctl eval "bind = SUPER,K,layoutmsg,cycleprev"
-  notify-send -e -u low -i "$notif" " Master Layout"
-  ;;
-*) ;;
+  master)
+    hyprctl eval 'hl.config({ general = { layout = "dwindle" } })'
+    notify-send -e -u low -i "$notif" "Dwindle Layout"
+    ;;
+  dwindle)
+    hyprctl eval 'hl.config({ general = { layout = "master" } })'
+    notify-send -e -u low -i "$notif" "Master Layout"
+    ;;
+  *)
+    notify-send -e -u normal -i "$notif" "Layout" "Unknown layout: $LAYOUT"
+    exit 1
+    ;;
 esac
